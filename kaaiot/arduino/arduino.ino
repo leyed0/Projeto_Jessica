74448b0c5990c8ee9ba1fedfa249960cc78626d1
leyed0 <orley1994@hotmail.com>
2021-03-01 10:48:06 -0300

    start comm

---
+++ kaaiot/arduino/arduino.ino
@@ -0,0 +1,13 @@
+#include<arduino.h>
+#include <SoftwareSerial.h>
+#include"cd4051.hpp"
+
+SoftwareSerial ESP(2,3);
+
+void setup(){
+
+}
+
+void loop(){
+
+}
---
+++ kaaiot/esp/arduino.ino
@@ -0,0 +1,13 @@
+#include<arduino.h>
+#include <SoftwareSerial.h>
+
+//GPIO 10, 9
+SoftwareSerial ESP(12,11);
+
+void setup(){
+
+}
+
+void loop(){
+
+}
