2b2d6e6b43c66f418cde6adbc7e5c017409d217b
leyed0 <orley1994@hotmail.com>
2021-03-04 19:37:20 -0300

    variavel

--- Teste_comunicação/teste_com_esp/teste_com_esp.ino
+++ Teste_comunicação/teste_com_esp/teste_com_esp.ino
@@ -14,16 +14,14 @@ void setup(){
     Serial1.begin(115200);
 }
 
-int a, b, op, r, c;
-
 void loop(){
     if(ReceiveData()){
         Serial.print("received: ");
-        Serial.print(a);
-        Serial.print(op==0?" + ":op==1?" - ":op==2?" * ":" / ");
-        Serial.print(b);
+        Serial.print(Mat.a);
+        Serial.print(Mat.op==0?" + ":Mat.op==1?" - ":Mat.op==2?" * ":" / ");
+        Serial.print(Mat.b);
         Serial.print(" = ");
-        Serial.println(r);
+        Serial.println(Mat.r);
     }
 }
 
@@ -32,7 +30,7 @@ bool ReceiveData(){
     {
         char* dp = (char*) &Mat;
         for (int i = 0; i < sizeof(mat); i++) *dp++ = Serial.read();
-        
+        Serial.read();
         return true;
     }
     return false;
