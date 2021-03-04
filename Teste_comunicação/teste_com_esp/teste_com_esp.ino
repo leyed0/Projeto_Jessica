d922282a8f20912958ec95cc97e3d0be79133bd1
leyed0 <orley1994@hotmail.com>
2021-03-04 18:08:31 -0300

    error fix

--- Teste_comunicação/teste_com_esp/teste_com_esp.ino
+++ Teste_comunicação/teste_com_esp/teste_com_esp.ino
@@ -1,21 +1,21 @@
-#include <ESP8266WiFi.h>
+#include <ARD8266WiFi.h>
 #include<SoftwareSerial.h>
 
-SoftwareSerial ESP(DD0, DD1);
+SoftwareSerial ARD(DD0, DD1);
 
 void setup(){
     Serial.begin(115200);
-    ESP.begin(115200);
+    ARD.begin(115200);
 }
 
 int a, b, op, r, c;
 
 void loop(){
-    if(ESP.available()){
-        a = ESP.read();
-        b = ESP.read();
-        op = ESP.read();
-        r = ESP.read();
+    if(ARD.available()){
+        a = ARD.read();
+        b = ARD.read();
+        op = ARD.read();
+        r = ARD.read();
 
         
         Serial.print(a);
