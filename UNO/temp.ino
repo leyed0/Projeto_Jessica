9deeaa425b9759767e79e7052433c5105d2dcaa5
Orley Rambo <orley1994@hotmail.com>
2021-09-07 18:28:36 -0300

    uno 2

--- UNO/temp.ino
+++ UNO/temp.ino
@@ -14,10 +14,6 @@
 
 #include "max6675.h" // max6675.h file is part of the library that you should download from Robojax.com
 
-int soPin = 5;// SO=Serial Out
-int csPin = 6;// CS = chip select CS pin
-int sckPin = 7;// SCK = Serial Clock pin
-
 //MAX6675 robojax(sckPin, csPin, soPin);// create instance object of MAX6675
 
 MAX6675 robojax1(7, 8, 6);// create instance object of MAX6675
@@ -25,40 +21,39 @@ MAX6675 robojax2(7, 9, 6);// create instance object of MAX6675
 MAX6675 robojax3(7, 10, 6);// create instance object of MAX6675
 MAX6675 robojax4(7, 11, 6);// create instance object of MAX6675
 MAX6675 robojax5(7, 12, 6);// create instance object of MAX6675
-
+int humidity = A0
+int time=1000;
 
 void setup() {
 
           
   Serial.begin(9600);// initialize serial monitor with 9600 baud
-  Serial.println("Robojax MAX6675"); 
+  Serial.println("Inicializado!"); 
 
 }
 
-void loop() {
-  // basic readout test, just print the current temp
-  // Robojax.com MAX6675 Temperature reading on Serial monitor
-   Serial.print("1 - C = "); 
-   Serial.print(robojax1.readCelsius());
-   Serial.print(" F = ");
-   Serial.println(robojax1.readFahrenheit());
-   Serial.print("2 - C = "); 
-   Serial.print(robojax2.readCelsius());
-   Serial.print(" F = ");
-   Serial.println(robojax2.readFahrenheit());
-   Serial.print("3 - C = "); 
-   Serial.print(robojax3.readCelsius());
-   Serial.print(" F = ");
-   Serial.println(robojax3.readFahrenheit());
-   Serial.print("4 - C = "); 
-   Serial.print(robojax4.readCelsius());
-   Serial.print(" F = ");
-   Serial.println(robojax4.readFahrenheit());
-   Serial.print("5 - C = "); 
-   Serial.print(robojax5.readCelsius());
-   Serial.print(" F = ");
-   Serial.println(robojax5.readFahrenheit());
 
-             
-   delay(1000);
+unsigned long timer = millis();
+void loop() {
+    
+    if(timer+time<=millis()){
+        timer+=time;
+        log()
+    }
+
+}
+
+void log(){
+   Serial.print("sensores: \t")
+   Serial.print(robojax1.readCelsius());
+   Serial.print("\t");
+   Serial.print(robojax2.readCelsius());
+   Serial.print("\t");
+   Serial.print(robojax3.readCelsius());
+   Serial.print("\t");
+   Serial.print(robojax4.readCelsius());
+   Serial.print("\t");
+   Serial.print(robojax5.readCelsius());
+   Serial.print("\t");
+   Serial.print(analogRead(humidity));
 }
